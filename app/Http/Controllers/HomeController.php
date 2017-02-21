f5f94500382d12d558041bf502d5b002bfa6fe07
Rob Andrews <rob@aphlor.org>
2017-02-21 23:57:06 +0000

    now we have content management, use it for home
    this will send root url (/) requests to a magic content page named
    'home' if it exists, otherwise to the property list.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Content;
+
 class HomeController extends Controller
 {
     /**
@@ -24,7 +26,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // until we decide on a dashboard layout, go straight to properties
-        return redirect()->route('properties');
+        $content = Content::where('path', 'home')->first();
+
+        // check if there's a content page for this. if not, go straight to property list
+        if ($content === null) {
+            // until we decide on a dashboard layout, go straight to properties
+            return redirect()->route('properties');
+        }
+
+        // great, there's a content page called "home". let's go to it.
+        return redirect()->route('viewcontent', 'home');
     }
 }
