9470087790f32c816763ff2176710225373009c9
Rob Andrews <rob@aphlor.org>
2017-04-15 10:16:01 +0100

    fix missing namespace for role derivation (closes #2)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
+use App\Models\Role;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
