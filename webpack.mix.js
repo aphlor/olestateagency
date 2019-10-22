53c009896e701dad8937919828ad46f7e80082ec
Rob Andrews <rob.andrews@mayden.co.uk>
2019-10-22 11:03:15 +0100

    update deps to remove security notices

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,9 +7,8 @@ const { mix } = require('laravel-mix');
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
+ | file for your application, as well as bundling up your JS files.
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js').sass('resources/assets/sass/app.scss', 'public/css');
